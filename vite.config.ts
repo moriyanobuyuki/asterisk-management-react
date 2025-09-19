9cfcd2bd44fd53998bb68065c6e2cc809692281b
NobuyukiMoriya <nobuyuki_moriya@aidma-hd.jp>
2025-09-19 17:06:48 +0900

    react+typscript環境構築

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
 
+// Vite の設定を TypeScript で定義
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.tsx',
             refresh: true,
         }),
-        tailwindcss(),
+        react(),
     ],
 });
